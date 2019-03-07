2c739431dba7b0b05096003411c8363921ee8015
Ekenlat <agliareth_ekenlat@hotmail.com>
2019-03-07 11:34:37 -0600

    Pull with ignore and a tag, not staging a file

--- src/main/java/com/codewars/challenges/Algorithms.java
+++ src/main/java/com/codewars/challenges/Algorithms.java
@@ -17,4 +17,114 @@ public class Algorithms
 
         return (int)arr.stream().filter(x -> x.matches(pattern)).count();
     }
+
+    public boolean isValid(char[] walk) {
+        int north = 0, south = 0, east = 0, west = 0;
+        if(walk.length == 10){
+            for (int i = 0; i < walk.length; i++) {
+                switch (walk[i]) {
+                    case 'n':
+                        north++;
+                        break;
+                    case 's':
+                        south++;
+                        break;
+                    case 'e':
+                        east++;
+                        break;
+                    case 'w':
+                        west++;
+                        break;
+                }
+            }
+            if (north == south && east == west)
+                return true;
+        }
+        return false;
+    }
+
+    public int reverseInt(int n) {
+        int reversed = 0;
+        while(n != 0){
+            reversed = reversed * 10 + (n % 10);
+            n /= 10;
+        }
+        return reversed;
+    }
+
+    public boolean feast(String beast, String dish) {
+        char lastChar = beast.charAt(beast.length()-1);
+        char firstChar = beast.charAt(0);
+
+        System.out.println(lastChar);
+
+        return dish.charAt(0) == firstChar && dish.charAt(dish.length()-1) == lastChar;
+
+    }
+
+    public String declareWinner(Fighter fighter1, Fighter fighter2, String firstAttacker) {
+        Fighter attacker = (fighter1.name.equals(firstAttacker)) ? fighter1 : fighter2;
+        Fighter defender = (attacker.equals(fighter2)) ? fighter1 : fighter2;
+        Fighter temp = null;
+
+        while(fighter1.health > 0 || fighter2.health > 0) {
+            defender.health -= attacker.damagePerAttack;
+
+            temp = defender;
+            defender = attacker;
+            attacker = temp;
+        }
+
+        return temp.name;
+    }
+
+    public int arithmetic(int a, int b, String operator) {
+        switch (operator) {
+        case "add":
+            System.out.println(a+b);
+            return a + b;
+        case "subtract":
+            System.out.println(a - b);
+            return a - b;
+        case "multiply":
+            System.out.println(a * b);
+            return a * b;
+        default:
+            System.out.println(a / b);
+            return a / b;
+        }
+    }
+    
+    public String makeReadable(int time) {
+        if(time == 0)
+            return "00:00:00";
+
+        String hours = "00";
+        String minutes = "00";
+        String seconds = "00";
+        int remainder = time % 3600;
+        
+        if(time/3600 != 0) {
+            if (time / 3600 < 10)
+                hours = "0" + time / 3600;
+            else
+                hours = String.valueOf(time / 3600);
+        }
+
+        if(remainder/60 != 0) {
+            if (remainder / 60 < 10)
+                minutes = "0" + remainder / 60;
+            else
+                minutes = String.valueOf(remainder / 60);
+        }
+         remainder = remainder % 60;
+
+         if(remainder != 0) {
+            if (remainder < 10)
+                seconds = "0" + remainder;
+            else
+                seconds = String.valueOf(remainder);
+         }
+        return String.format("%s:%s:%s", hours, minutes, seconds);
+    }
 }
--- src/test/java/com/codewars/challenges/AlgorithmsTest.java
+++ src/test/java/com/codewars/challenges/AlgorithmsTest.java
@@ -1,6 +1,8 @@
 package com.codewars.challenges;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -58,5 +60,42 @@ public class AlgorithmsTest {
         a.add(":~D");
         a.add(";-~D");
         assertEquals(4, algorithms.countSmileys(a));
+    }
+
+    @Test
+    public void Test() {
+        assertTrue(algorithms.isValid(new char[] { 'n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's' }));
+        assertFalse(algorithms.isValid(new char[] { 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e' }));
+        assertFalse(algorithms.isValid(new char[] { 'w' }));
+        assertFalse(algorithms.isValid(new char[] { 'n', 'n', 'n', 's', 'n', 's', 'n', 's', 'n', 's' }));
+    }
+    @Test
+    public void test200() {
+        assertEquals(54321, algorithms.reverseInt(12345));
+    }
+    @Test
+    public void fixedTest() {
+        assertTrue(algorithms.feast("great blue heron", "garlic nann"));
+        assertTrue(algorithms.feast("chickadee", "chocolate cake"));
+        assertFalse(algorithms.feast("brown bear", "bear claw"));
+    }
+    
+    @Test
+    public void testBasic() {
+        assertEquals(3,
+                algorithms.arithmetic(1, 2, "add"));
+        assertEquals(6, algorithms.arithmetic(8, 2, "subtract"));
+        assertEquals(10,
+                algorithms.arithmetic(5, 2, "multiply"));
+        assertEquals(4, algorithms.arithmetic(8, 2, "divide"));
+    }
+
+    @Test
+    public void Tests() {
+        assertEquals("00:00:05", algorithms.makeReadable(5));
+        assertEquals("00:01:00", algorithms.makeReadable(60));
+        assertEquals("00:00:00", algorithms.makeReadable(0));
+        assertEquals("23:59:59", algorithms.makeReadable(86399));
+        assertEquals("99:59:59", algorithms.makeReadable(359999));
     }
 }
